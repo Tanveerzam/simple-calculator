e84a263033930f89e1447b78551c7c6a99e04eef
Tanveerzam <106382525+Tanveerzam@users.noreply.github.com>
2023-10-10 23:50:03 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from addition import add
+from subtraction import subtract
+from multiply import multiply
+import pandas as pd
+if __name__=="__main__":
+    a=6
+    b=4
+    print(add(a,b))
+    print(subtract(a,b))
+    print(multiply(a,b))
+
+    
